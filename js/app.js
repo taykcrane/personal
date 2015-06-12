a205e8f774679e07e9fa6bac4fff838e6ad67e0a
Taylor Crane <taylorkcrane@gmail.com>
2015-06-12 15:33:35 -0700

    beginning to animate the email box

--- js/app.js
+++ js/app.js
@@ -5,3 +5,10 @@ $(document).ready(function () {
 
 
 })
+
+function openEmailBox () {
+	$(".email-box").css("display", "inline-block");
+	setTimeout(function () {
+		$(".email-box").toggleClass("email-box-animation");
+	}, 10);
+}
